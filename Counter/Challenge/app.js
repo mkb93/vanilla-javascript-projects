d75af812d73ce0c365f6646b9b2158bd5aa63f6b
marco belahouane <nfk18@hotmail.co.uk>
2023-11-13 18:54:12 +0000

    challenge make a space for the buttons to increase a random number in the card

--- Counter/Challenge/app.js
+++ Counter/Challenge/app.js
@@ -8,3 +8,22 @@ btn1.addEventListener('click',()=>{
 btn2.addEventListener('click',()=>{
   card.style.transform = 'rotateY(0deg)';
 })
+let count = 0;
+const btns = document.querySelectorAll('.btn')
+const value = document.getElementById('value')
+
+btns.forEach((btn)=>{
+  btn.addEventListener('click',(e)=>{
+    const styles = e.currentTarget.classList;
+    if (styles.contains('decrease')){
+      count--;
+    }
+    else if (styles.contains('increase')){
+      count++;
+    }
+    else {
+      count = 0;
+    }
+    value.textContent = count;
+  })
+})
