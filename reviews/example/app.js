cd6772ae7561e4ff9cba19a46bfdaa9e0be37394
marco belahouane <nfk18@hotmail.co.uk>
2023-11-15 12:00:10 +0000

    finished reviews example project

--- reviews/example/app.js
+++ reviews/example/app.js
@@ -35,14 +35,40 @@ const job =document.getElementById('job');
 const info =document.getElementById('info');
 
 const prevBtn = document.querySelector('.prev-btn')
-const nextBtn = document.querySelector('next-btn.')
+const nextBtn = document.querySelector('.next-btn')
 const randomBtn = document.querySelector('.random-btn')
-
 // set starting value
 let currentItem=0;
 //load intial item
-window.addEventListener("DOMContentLoaded", ()=>{
-  const item = reviews[currentItem];
+function showPerson(person) {
+  const item = reviews[person];
   img.src = item.img;
-
+  author.textContent = item.name;
+  job.textContent =item.job;
+  info.textContent = item.text;
+}
+window.addEventListener("DOMContentLoaded", ()=>{
+ showPerson(currentItem);
+})
+prevBtn.addEventListener('click', ()=>{
+  if(currentItem > 0 && currentItem< reviews.length ){
+    currentItem--;
+  }
+  else if(currentItem == 0){
+    currentItem = reviews.length-1
+  }
+  showPerson(currentItem)
+})
+nextBtn.addEventListener('click', ()=>{
+  if(currentItem >= 0 && currentItem< reviews.length-1 ){
+    currentItem++;
+  }
+  else if(currentItem == reviews.length-1){
+    currentItem = 0
+  }
+  showPerson(currentItem)
+})
+randomBtn.addEventListener('click', ()=>{
+  currentItem = Math.floor(Math.random()*reviews.length)
+  showPerson(currentItem)
 })
