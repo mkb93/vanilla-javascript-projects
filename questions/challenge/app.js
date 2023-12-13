efe0b04fd776006d68473106b060d2d51f639510
marco belahouane <nfk18@hotmail.co.uk>
2023-12-13 16:56:31 +0000

    made a modal appear based on click but doesnt look good

--- questions/challenge/app.js
+++ questions/challenge/app.js
@@ -1,13 +1,27 @@
 const listItems = document.querySelectorAll('ul')[0].querySelectorAll('li')
+const modalBtn = document.querySelector('.close-modal')
+
+
+
 
 listItems.forEach(item=>{
   item.addEventListener('click',e=>{
     listItems.forEach(otherItem =>{
       if(item !== otherItem){
         otherItem.classList.remove('selected')
+        otherItem.querySelector('div').classList.add('question-text')
       }
+      
+    })
+    modalBtn.addEventListener('click',e=>{
+      modalBtn.parentElement.parentElement.classList.remove('visible');
+      document.querySelector('.overlay').classList.remove('visible');
+      document.querySelector('body').classList.toggle('active-modal')
     })
     item.classList.toggle('selected')
-
+    // item.querySelector('div').classList.toggle('question-text')
+  //  document.querySelector('body').classList.toggle('active-modal')
+   modalBtn.parentElement.parentElement.classList.toggle('visible');
+   document.querySelector('.overlay').classList.toggle('visible');
   })
 })
